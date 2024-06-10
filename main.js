2e0f36c883ab50a90de2f205525e00e6ddc6016f
Braian Palacios <braianpalacios699@gmail.com>
2024-06-09 21:00:20 -0300

    Se optimizo un poco el codigo en archivo .ts y se comentaron detalles de la logica.

--- main.js
+++ main.js
@@ -1,28 +1,22 @@
-var titulo = document.querySelector(".contenedor-principal__titulo");
+var titulo = document.querySelector(".contenedor-principal__titulo"); //llamo identificador por su selector del H1
 var palabra = "La banda del discord";
 var texto = "";
 var contadorLetras = 0;
 var estaBorrando = false;
 // Funcion que se encarga de deletrear el texto del titulo
 function escribirTitulo() {
-    if (!estaBorrando) {
-        texto += palabra[contadorLetras];
-        titulo.textContent = texto;
-        contadorLetras++;
-        if (contadorLetras === palabra.length) {
-            estaBorrando = true;
-            contadorLetras--;
-        }
-    }
+    if (!estaBorrando && contadorLetras < palabra.length)
+        texto += palabra[contadorLetras++]; // SI NO ESTA BORRANDO Y SI CONTADOR LETRAS ES MENOR A LONGITUD DE PALABRA
+    else if (estaBorrando && contadorLetras > 0)
+        texto = texto.slice(0, contadorLetras--); // SI ESTA BORRANDO Y SI CONTADOR LETRAS ES MAYOR A CERO
+    // SINO ACTUAR EN CONSECUENCIA
     else {
-        texto = texto.substring(0, contadorLetras);
-        titulo.textContent = texto;
-        contadorLetras--;
-        if (contadorLetras === 0) {
-            estaBorrando = false;
-            contadorLetras = 1;
-        }
+        estaBorrando = !estaBorrando; //GUARDA LO CONTRARIO A LO QUE VENGA EN BOOLEANO
+        //CONTADOR LETRAS GUARDARA EL VALOR NUMERICO SEGUN LA CONDICION TERNARIA SI ESTA BORRANDO O NO.
+        contadorLetras = estaBorrando ? contadorLetras : contadorLetras + 1;
     }
+    // PONEMOS EL TEXTO EN EL CONTENIDO DEL ELEMENTO HTML H1
+    titulo.textContent = texto;
 }
-// funcion de intervalo de tiempo
+// funcion de intervalo de tiempo para mostrar en 200 milisegundo cada caracter del texto.
 setInterval(escribirTitulo, 200);
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-const titulo = document.querySelector(".contenedor-principal__titulo") as HTMLElement;
+const titulo = document.querySelector(".contenedor-principal__titulo") as HTMLElement; //llamo identificador por su selector del H1
 let palabra: string = "La banda del discord";
 let texto: string = "";
 let contadorLetras: number = 0;
@@ -6,25 +6,18 @@ let estaBorrando: boolean = false;
 
 // Funcion que se encarga de deletrear el texto del titulo
 function escribirTitulo(){
-    if (!estaBorrando) {
-        texto += palabra[contadorLetras];
-        titulo.textContent=texto;
-        contadorLetras++;
-        if (contadorLetras === palabra.length) {
-            estaBorrando = true;
-            contadorLetras--;
-        }
-    } else {
-        texto = texto.substring(0, contadorLetras);
-        titulo.textContent=texto;
-        contadorLetras--;
-        if (contadorLetras === 0) {
-            estaBorrando = false;
-            contadorLetras = 1;
-        }
+    if (!estaBorrando && contadorLetras < palabra.length) texto += palabra[contadorLetras++] // SI NO ESTA BORRANDO Y SI CONTADOR LETRAS ES MENOR A LONGITUD DE PALABRA
+    else if (estaBorrando && contadorLetras > 0) texto = texto.slice(0, contadorLetras--); // SI ESTA BORRANDO Y SI CONTADOR LETRAS ES MAYOR A CERO
+    // SINO ACTUAR EN CONSECUENCIA
+    else{
+        estaBorrando = !estaBorrando; //GUARDA LO CONTRARIO A LO QUE VENGA EN BOOLEANO
+        //CONTADOR LETRAS GUARDARA EL VALOR NUMERICO SEGUN LA CONDICION TERNARIA SI ESTA BORRANDO O NO.
+        contadorLetras= estaBorrando ? contadorLetras : contadorLetras + 1;  
     }
+    // PONEMOS EL TEXTO EN EL CONTENIDO DEL ELEMENTO HTML H1
+    titulo.textContent=texto;
 }
-// funcion de intervalo de tiempo
+// funcion de intervalo de tiempo para mostrar en 200 milisegundo cada caracter del texto.
 setInterval(escribirTitulo, 200);
 
 
